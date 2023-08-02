72f7c2c8e44555b2ed518e40cea5ad38cb577800
nanup <nanup.personal+github@gmail.com>
2023-08-02 14:47:42 +0530

    type guards

--- Advanced Types/src/app.ts
+++ Advanced Types/src/app.ts
@@ -15,3 +15,18 @@ const e1: ElevatedEmployeed = {
   priveleges: ['create server'],
   startDate: new Date(),
 };
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' && typeof b === 'string') {
+    return a.toString() + b.toString();
+  } else {
+    return a + b;
+  }
+}
+
+type
