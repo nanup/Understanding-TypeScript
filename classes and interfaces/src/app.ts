cc9a39d3092688c6e2dde3f57945f88a0c3a954e
nanup <nanup.personal+github@gmail.com>
2023-08-02 14:33:18 +0530

    final

--- classes and interfaces/src/app.ts
+++ classes and interfaces/src/app.ts
@@ -1,8 +1,21 @@
 interface Person {
-  name: string;
+  readonly name: string;
   age: number;
 
   greet(phrase: string): void;
+}
+
+class User implements Person {
+  name: string;
+  age = 30;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string) {
+    console.log(phrase);
+  }
 }
 
 let user1: Person;
