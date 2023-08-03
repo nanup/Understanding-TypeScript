e3da6e2bd82a16defc7c1282aeffd331237faffb
nanup <nanup.personal+github@gmail.com>
2023-08-03 19:08:04 +0530

    default generics

---
+++ Generics/src/app.ts
@@ -0,0 +1,8 @@
+// const names = ['Max', 'Manuel'];
+// const places: Array<string> = [];
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!');
+//   }, 2000);
+// });
