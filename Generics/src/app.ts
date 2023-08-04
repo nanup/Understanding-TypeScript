a70c10ddde0112f8e42efa2d614cefe7c8bd423f
nanup <nanup.personal+github@gmail.com>
2023-08-04 15:35:42 +0530

    generics

--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -6,3 +6,24 @@
 //     resolve('This is done!');
 //   }, 2000);
 // });
+
+function merge<T extends Object, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: 'Nanu', hobbies: 'cinema' }, { age: 25 });
+console.log(mergedObj.name);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length > 0) {
+    descriptionText = 'Got ' + element.length + 'elements.';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
